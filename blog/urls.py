a44922482d8cb1a589efad5709e7befa950e368f
davronovuz <davronovtatu@gmail.com>
2025-07-28 10:02:54 +0500

    kompyuter tanlash

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import HomeView,ContactView
-
-urlpatterns = [
-    path('',HomeView.as_view(),name="bsahifa"),
-    path('contact/',ContactView.as_view(),name="aloqa"),
-]
